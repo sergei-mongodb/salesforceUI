7d1aa426f7fe6f856e4cf3096a7b537e93c6c7d3
Sergei <87734261+sergei-mongodb@users.noreply.github.com>
2022-02-15 22:41:15 -0800

    Update salesforceUI.js

--- salesforceUI.js
+++ salesforceUI.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.3
 // @description  try to take over the world!
-// @author       You
+// @author       Sergei Timoshtshuk, Joe Caswell
 // @match        https://mongodb.lightning.force.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=force.com
 // @grant        none
